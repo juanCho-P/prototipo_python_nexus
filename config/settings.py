a12f4285013d96a83d5f61381f9daf9cfb896cba
juan pablo sanchez <juanpablo9033@hotmail.com>
2026-08-31 18:41:42 -0500

    fix: update EMAIL_BACKEND path to use users.services.BrevoEmailBackend

--- config/settings.py
+++ config/settings.py
@@ -170,7 +170,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = 'login'
 #configuracion Email
-EMAIL_BACKEND = 'core.email_backend.BrevoEmailBackend'
+EMAIL_BACKEND = 'users.services.BrevoEmailBackend'
 DEFAULT_FROM_EMAIL = 'equiponexus687@gmail.com'
 
 
