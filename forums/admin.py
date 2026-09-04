91802c9bb5d56b0f95b6d73012644d4c1ad28963
juan pablo sanchez <juanpablo9033@hotmail.com>
2026-09-04 15:34:23 -0500

    fix: update ComentarioAdmin to display 'respuesta' and search by 'id_usuario__email'

--- forums/admin.py
+++ forums/admin.py
@@ -10,8 +10,8 @@ class ForoAdmin(admin.ModelAdmin):
    
 @admin.register(Comentario)
 class ComentarioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'id_foro', 'id_usuario', 'activo', 'created_at', 'has_image','contenido')
-    search_fields = ('respuesta', 'id_usuario__username')
+    list_display = ('id', 'id_foro', 'id_usuario', 'activo', 'created_at', 'has_image', 'respuesta')
+    search_fields = ('respuesta', 'id_usuario__email')
     list_filter = ('activo', 'created_at')
 
     @admin.display(boolean=True, description='¿Imagen?')
